424ac64023846239d69a8a3cfa9dfe9d6795935a
Jonathan Lövgren <jonathan.lovgren@skola.sala.se>
2024-10-24 09:36:03 +0200

    Add project files.

---
+++ Minesweeper/Controller.cs
@@ -0,0 +1,164 @@
+﻿using Minesweeper.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    internal class Controller
+    {
+        private Form1 form;
+       public int tag;
+
+        public Controller(Form1 formInstance)
+        {
+            form = formInstance;
+        }
+
+        internal void Sweep(int col, int row)
+        {
+
+            CheckForWin();
+            if (CheckForWin() == true)
+            {
+                
+                tag = 0;
+                MessageBox.Show("WINNER");
+            }
+            if (form.gameOver || form.grid[col, row].Tag != null)
+            {
+
+                return;
+            }
+
+            int mineCount = 0;
+
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+
+                    if (x == 0 && y == 0) continue;
+
+                    int newCol = col + x;
+                    int newRow = row + y;
+
+
+                    if (newCol >= 0 && newCol < 10 && newRow >= 0 && newRow < 10)
+                    {
+
+                        if (form.grid[newCol, newRow].Tag != null && form.grid[newCol, newRow].Tag.ToString() == "Mine")
+                        {
+                            mineCount++;
+                        }
+                    }
+                }
+            }
+
+
+            form.grid[col, row].Tag = "Revealed";
+            tag++;
+
+            if (mineCount == 0)
+            {
+                form.grid[col, row].Image = Resources.Blank;
+
+
+                for (int x = -1; x <= 1; x++)
+                {
+                    for (int y = -1; y <= 1; y++)
+                    {
+                        if (x == 0 && y == 0) continue;
+                        int newCol = col + x;
+                        int newRow = row + y;
+
+
+                        if (newCol >= 0 && newCol < 10 && newRow >= 0 && newRow < 10)
+                        {
+
+                            if (form.grid[newCol, newRow].Tag == null)
+                            {
+                                Sweep(newCol, newRow);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+                if (mineCount == 1)
+                {
+                    form.grid[col, row].Image = Resources._1;
+                }
+                if (mineCount == 2)
+                {
+                    form.grid[col, row].Image = Resources._2;
+                }
+                if (mineCount == 3)
+                {
+                    form.grid[col, row].Image = Resources._3;
+                }
+                if (mineCount == 4)
+                {
+                    form.grid[col, row].Image = Resources._4;
+                }
+                if (mineCount == 5)
+                {
+                    form.grid[col, row].Image = Resources._5;
+                }
+                if (mineCount == 6)
+                {
+                    form.grid[col, row].Image = Resources._6;
+                }
+                if (mineCount == 7)
+                {
+                    form.grid[col, row].Image = Resources._7;
+                }
+                if (mineCount == 8)
+                {
+                    form.grid[col, row].Image = Resources._8;
+                }
+                
+            }
+        }
+
+
+        public void PlaceMines(int numberOfMines)
+        {
+            Random rand = new Random();
+            int minesPlaced = 0;
+
+            while (minesPlaced < numberOfMines)
+            {
+                int randRow = rand.Next(0, 10);
+                int randCol = rand.Next(0, 10);
+
+
+                if (form.grid[randCol, randRow].Tag == null) //om det inte finns en tag lägger det till en tag med texten Mine
+                {
+                    form.grid[randCol, randRow].Tag = "Mine";
+                    tag++;
+                    minesPlaced++;
+                }
+            }
+        }
+        public bool CheckForWin()
+        {
+            if (tag == 100)
+            {
+               
+                return true;
+               
+            }
+            else return false;
+        }
+    }
+}
+
---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,133 @@
+﻿using Minesweeper.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+       
+    
+       
+        Controller control;
+        public PictureBox[,] grid = new PictureBox[10, 10]; //ops kommer bara fungera med 10 x 10...
+       public bool gameOver = false;
+        int tid = 0;
+        public Form1()
+        {
+           
+           InitializeComponent();
+            InitializeGrid();
+         
+            control = new Controller(this); 
+            control.PlaceMines(10);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void InitializeGrid()
+        {
+            
+           
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    PictureBox lbl = new PictureBox();
+                    lbl.Size = new Size(40, 40);
+                    lbl.Location = new Point(x * 40, y * 40);
+                    lbl.SizeMode = PictureBoxSizeMode.StretchImage;
+                    lbl.Image = Resources.Unrevealed;
+                   
+                    
+                    lbl.Click += new EventHandler(Label_Click);
+                    grid[x, y] = lbl;
+                    Controls.Add(lbl);
+                }
+            }
+        }
+        public void Label_Click(object sender, EventArgs e)
+        {
+          
+            if (gameOver) return;
+           if (control.CheckForWin() == true)
+            {
+            
+            }
+            PictureBox clickedLabel = sender as PictureBox;
+            int row = (clickedLabel.Location.Y / 40);
+            int col = (clickedLabel.Location.X / 40);
+            control.Sweep(col, row);
+            if (clickedLabel.Tag != null && clickedLabel.Tag.ToString() == "Mine")
+            {
+             
+                for (int x = 0; x <10; x++)
+                {
+                    for (int y = 0; y < 10; y++)
+                    {
+                        if (grid[x,y].Tag == "Mine")
+                        {
+                            grid[x, y].Image = Resources.Bomb;
+                        }
+                    }
+                    button1.BackgroundImage = Resources.Dead;
+                    control.tag = 0;
+                   gameOver = true;
+                }
+                MessageBox.Show("Boom! Game Over!");
+            }
+
+            else
+            {
+               control.Sweep(col, row);
+            }
+          
+             
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = control.tag.ToString();
+            tid++;
+            label1.Text = tid.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+            tid = 0; label1.Text = "0";
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    if (grid[x, y].Tag != null)
+                    {
+                        grid[x, y].Tag = null;
+                        grid[x, y].Image = Resources.Unrevealed;
+                    }
+                }
+               
+            }
+            gameOver = false;
+            button1.BackgroundImage = Resources.smile;
+            control.PlaceMines(10);
+        }
+       
+
+
+    }
+}
+
+ 
